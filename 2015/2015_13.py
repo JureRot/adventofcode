c96d3a04a45d58eddb9e9b03bcefe3a754b21149
NeumnoTele <jure.rot96@gmail.com>
2018-02-11 02:38:06 +0100

    2015_14

--- 2015/2015_13.py
+++ 2015/2015_13.py
@@ -11,43 +11,39 @@ best_arr = []
 happ_change = 0
 
 
-with open('input2015_13.txt', 'r') as myfile:
-	input = myfile.read().split('\n')
-
-
 
 for i in input:
-	i_list = i.split(' ')
+	i_list = i.split(' ') #splits using spaces
 
-	if (i_list[2] == "gain"):
-		people[i_list[0]][i_list[10][:-1]] = int(i_list[3])
-	elif (i_list[2]== "lose"):
-		people[i_list[0]][i_list[10][:-1]] = -1 * int(i_list[3])
+	if (i_list[2] == "gain"): #if gain
+		people[i_list[0]][i_list[10][:-1]] = int(i_list[3]) #adds the value at this person for this personn
+	elif (i_list[2]== "lose"): #if lose
+		people[i_list[0]][i_list[10][:-1]] = -1 * int(i_list[3]) #adds negative
 
-pk = itertools.permutations(people.keys())
+pk = itertools.permutations(people.keys()) #makes permutations of all the people (order of all the people)
 
-for i in pk:
+for i in pk: #for every permutaton possible
 	temp_change = 0
-	for j in range(len(i)):
-		if (j == 0):
+	for j in range(len(i)): #goes over all people
+		if (j == 0): #for the first one we must add the last one
 			temp_change += people[i[j]][i[len(i)-1]]
 			temp_change += people[i[j]][i[j+1]]
-		elif (j == len(i)-1):
+		elif (j == len(i)-1): #for the last one we must add the first one
 			temp_change += people[i[j]][i[j-1]]
 			temp_change += people[i[j]][i[0]]
-		else:
+		else: #for in between we have ne problem
 			temp_change += people[i[j]][i[j-1]]
 			temp_change += people[i[j]][i[j+1]]
 
 	
-	if (temp_change > happ_change):
+	if (temp_change > happ_change): #if total betterr than the best till no, we change
 		happ_change = temp_change
 		best_arr = i
 
 print("1. the total change of happines for the best arragement: ", happ_change)
 
 
-#second part (including myself)
+#second part (including myself) (just add our selves with all zeros, repeat the whole process)
 best_arr2 = []
 happ_change2 = 0
 
--- 2015/2015_14.py
+++ 2015/2015_14.py
@@ -2,7 +2,15 @@ import collections
 
 
 #starting vars
-deer = collections.defaulfdict(dict())
+deer = collections.defaultdict(dict)
+best_deer = ""
+longest = 0
+
+total_time = 2503
+
+#second part
+best_deer2 = ""
+most_points = 0
 
 
 with open('input2015_14.txt', 'r') as myfile:
@@ -11,4 +19,54 @@ with open('input2015_14.txt', 'r') as myfile:
 
 for i in input:
 	i_list = i.split(' ')
-	print(i_list)
+	deer[i_list[0]]["speed"] = int(i_list[3])
+	deer[i_list[0]]["f_time"] = int(i_list[6])
+	deer[i_list[0]]["r_time"] = int(i_list[13])
+	deer[i_list[0]]["flying"] = True
+	deer[i_list[0]]["t_till_change"] = int(i_list[6])
+	deer[i_list[0]]["distance"] = 0
+	#second part
+	deer[i_list[0]]["points"] = 0
+
+
+def give_points(d):
+	most_p = max({k: v["distance"] for k, v in deer.items()}.values()) #find max of values in dict name:distance
+
+	for k, v in d.items():
+		if (v["distance"] == most_p):
+			d[k]["points"] += 1
+
+
+
+for i in range(1, total_time+1):
+	for k in deer.keys():
+		if (deer[k]["flying"]):
+			deer[k]["distance"] += deer[k]["speed"]
+
+		deer[k]["t_till_change"] -= 1
+
+		if (deer[k]["t_till_change"] == 0):
+			if (deer[k]["flying"]):
+				deer[k]["flying"] = False
+				deer[k]["t_till_change"] = deer[k]["r_time"]
+			else:
+				deer[k]["flying"] = True
+				deer[k]["t_till_change"] = deer[k]["f_time"]
+
+	give_points(deer)
+
+
+for k in deer.keys():
+	if (deer[k]["distance"] > longest):
+		longest = deer[k]["distance"]
+		best_deer = k
+
+print("1. total distance of the best deer: ", longest)
+
+
+for k in deer.keys():
+	if (deer[k]["points"] > most_points):
+		most_points = deer[k]["points"]
+		best_deer2 = k
+
+print("2. total points of the best deer according to points: ", most_points)
