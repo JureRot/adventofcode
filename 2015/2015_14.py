9085cf7505c95cd41d6cccf3fa19ae7ded446416
NeumnoTele <jure.rot96@gmail.com>
2018-02-11 02:42:28 +0100

    2015_14

--- 2015/2015_14.py
+++ 2015/2015_14.py
@@ -17,7 +17,7 @@ with open('input2015_14.txt', 'r') as myfile:
 	input = myfile.read().split('\n')
 
 
-for i in input:
+for i in input: #constructs a dict -> name: {speed:, f_time:, r_time:, flying:, t_till_change:, distance:, pints:}
 	i_list = i.split(' ')
 	deer[i_list[0]]["speed"] = int(i_list[3])
 	deer[i_list[0]]["f_time"] = int(i_list[6])
@@ -32,31 +32,31 @@ for i in input:
 def give_points(d):
 	most_p = max({k: v["distance"] for k, v in deer.items()}.values()) #find max of values in dict name:distance
 
-	for k, v in d.items():
+	for k, v in d.items(): #adds one point to everyone with the max_p equal to points (can be more than one)
 		if (v["distance"] == most_p):
 			d[k]["points"] += 1
 
 
 
-for i in range(1, total_time+1):
-	for k in deer.keys():
-		if (deer[k]["flying"]):
-			deer[k]["distance"] += deer[k]["speed"]
+for i in range(1, total_time+1): #for every second of the race (from 1, not 0)
+	for k in deer.keys(): #for every deer
+		if (deer[k]["flying"]): #if flying
+			deer[k]["distance"] += deer[k]["speed"] #adds to distance
 
-		deer[k]["t_till_change"] -= 1
+		deer[k]["t_till_change"] -= 1 #reduces one time
 
-		if (deer[k]["t_till_change"] == 0):
-			if (deer[k]["flying"]):
+		if (deer[k]["t_till_change"] == 0): #if time at 0
+			if (deer[k]["flying"]): #either make it fly
 				deer[k]["flying"] = False
 				deer[k]["t_till_change"] = deer[k]["r_time"]
-			else:
+			else: #or make it rest
 				deer[k]["flying"] = True
 				deer[k]["t_till_change"] = deer[k]["f_time"]
 
-	give_points(deer)
+	give_points(deer) #gives points to the deers with the most points
 
 
-for k in deer.keys():
+for k in deer.keys(): #finds deer traveled the longes
 	if (deer[k]["distance"] > longest):
 		longest = deer[k]["distance"]
 		best_deer = k
@@ -64,7 +64,7 @@ for k in deer.keys():
 print("1. total distance of the best deer: ", longest)
 
 
-for k in deer.keys():
+for k in deer.keys(): #finds the deer with the most ponts
 	if (deer[k]["points"] > most_points):
 		most_points = deer[k]["points"]
 		best_deer2 = k
