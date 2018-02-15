0c63d4f4bdd806a8fefaf0d677ad7323168c7ca3
NeumnoTele <jure.rot96@gmail.com>
2018-02-15 01:22:08 +0100

    2015_21 start

---
+++ 2015/2015_21.py
@@ -0,0 +1,12 @@
+
+
+
+with open('input2015_21.txt', 'r') as myfile:
+	input = myfile.read().split('\n')
+
+
+for i in input:
+	print(i)
+
+
+#idea: class za player pa boss, in pol mamo funkcijo battle ki postopoma obema zbija tocke glede na napad (mau za povadt classe v pythonu)
