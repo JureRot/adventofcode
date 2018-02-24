3198ccd07ea2614874393e1be82c693c89bc017f
NeumnoTele <jure.rot96@gmail.com>
2018-02-24 18:21:09 +0100

    2015_24 and take 2 done

--- 2015/2015_24.py
+++ 2015/2015_24.py
@@ -54,10 +54,8 @@ def get_subsets(in_set, sspm, t, max_i, subsets): #gets all subsets from subset 
 					r_set.add(i)
 
 		for i in r_set:
-			#print(r_set, str(in_set[i-1])+","+"("+str(t)+")")
 			subsets += str(in_set[i-1])+"/"+str(t)+"," #nuber chosen / sum at that point (next level of recur. will be for curr sum - number chosen)
 			if (t-in_set[i-1] == 0):
-				#print(";")
 				subsets += ";"
 			subsets = get_subsets(in_set, sspm, t-in_set[i-1], i, subsets)
 		
@@ -70,15 +68,6 @@ def get_subsets(in_set, sspm, t, max_i, subsets): #gets all subsets from subset 
 def decode_subsets(s, t): #gets weird string format of sets and outputs actual set of subsets
 	sets = []
 
-	"""l_set = s.split(';')[:-1] #the last element is an empty string (probably \n or something)
-
-	for i in l_set:
-		inset = []
-		m_set = i.split(',')[:-1] #same reason as above
-		for j in m_set:
-			s_set = j.split('/')
-			#print(s_set)"""
-
 	all_set = [[i.split('/') for i in i.split(',')[:-1]] for i in s.split(';')[:-1]]
 	#split string over ; (negating last elelment). splits that using , (negating last element). splits that using / (array inside array inside array)
 	#[:-1] is used on ; and , because there is the last empty element we dont want
@@ -88,87 +77,83 @@ def decode_subsets(s, t): #gets weird string format of sets and outputs actual s
 		inset = []
 
 		if (int(i[0][1]) == t): #if the branch happend at the beginning (sum at that point is the total sum we want)
-			inset = [int(n) for n,k in i]
+			inset = [int(n) for n,k in i] #we just write the whole sequence (as it is the whole subset)
 
 		else: #if branch in betwen (we need to prefill the inset)
-			n = int(i[0][1])
-			temp_t = t
+			n = int(i[0][1]) #the sum at branch
+			temp_t = t #the total branch
 
-			for j in sets[-1]:
+			for j in sets[-1]: #we use the last added inset (the first one is never not whole) to add the needed fill in the begining (before the branch)
 				inset.append(j)
-				temp_t -= j
+				temp_t -= j #differenciate until 0
 				if (temp_t == n):
-					break
+					break #we stop when we reach the branch
 
-			inset += [int(n) for n,k in i] #adds the remaining after branching
+			inset += [int(n) for n,k in i] #and we add the remaining after branching
 			
 
-		sets.append(inset)
+		sets.append(inset) #appends the set to the whole collection
 
 	return (sets)
 
 
 
-out = make_sspm(packages, t)
+out = make_sspm(packages, t) #we make the table for the subset sum problem (quite fast)
 
-"""f = open("input2015_24ssptable.txt", 'w')
-f.write(out)
-f.close()"""
-
-gs = get_subsets(packages, out, t, len(out), "") #holy shit this is slow (maybe candidate for take 2) takes about 50 min
+"""gs = get_subsets(packages, out, t, len(out), "") #we recursively backtrack over the table to create all the possible subsets
+#holy shit this is slow (maybe candidate for take 2) takes about 50 min (that is why we save it to a file (6MB) and than we read from it)
 
 f = open("input2015_24textsubsets3.txt", 'w')
 f.write(gs)
-f.close()
+f.close()"""
 
-with open('input2015_24textsubsets3.txt', 'r') as myfile: #closes by itself ???
+with open('input2015_24textsubsets3.txt', 'r') as myfile: #this form is used because it closes by itself (i guess)
 	text_subsets = myfile.read()
 
-ds = decode_subsets(text_subsets, t)
+ds = decode_subsets(text_subsets, t) #we decode the weird recursive text format that the get_subsets() creates into a normal set / list
 
-sorted_ds = sorted(ds, key=len)
+sorted_ds = sorted(ds, key=len) #we sort the subsets by lenght in increasing order
 
 min_num = None
 smallest = []
 
-for i in sorted_ds:
-	if (min_num == None):
+for i in sorted_ds: #we find the group of the shortest subsets
+	if (min_num == None): #first one will trigger this
 		min_num = len(i)
 		smallest.append(i)
 	else:
-		if (len(i) == min_num):
+		if (len(i) == min_num): #all other who are the same len as the first one will also go through
 			smallest.append(i)
 		else:
-			break
-#print(len(smallest))
+			break #any that are longer are rejected
+
 
 smallest_qe = sys.maxsize
 partition = []
 
-for i in smallest:
-	if (functools.reduce(operator.mul, i) < smallest_qe):
+for i in smallest: #within the group of the smallest subsets we find the one with the smallest quatum entagelement
+	if (functools.reduce(operator.mul, i) < smallest_qe): #map() applies func on every element, reduce() applies function on every element subsequently to the next one(func in this case takes 2 parameters, can be sum (mul is multiplication))
 		smallest_qe = functools.reduce(operator.mul, i)
 		partition = i
+
 
 print("1. the quantum entanglement of the bag with the smallest ammount of packages equating to the third and the smallest entanglement: ", smallest_qe)
 
 
-#second part
+#second part (same but with partitioning into fourths. this means we need to do everything again (theoretically we dont need to do the make_sspm again, but everything else, we doo))
 t = sum(packages) // 4
 
 out = make_sspm(packages, t)
 
-"""f = open("input2015_24ssptable.txt", 'w')
-f.write(out)
-f.close()"""
 
-gs = get_subsets(packages, out, t, len(out), "") #holy shit this is slow (maybe candidate for take 2) takes about 50 min
+"""gs = get_subsets(packages, out, t, len(out), "") #holy shit this is slow (maybe candidate for take 2) takes about 50 min (that is why we save it to a file (6MB) and than we read from it)
 
 f = open("input2015_24textsubsets4.txt", 'w')
 f.write(gs)
-f.close()
+f.close()"""
 
-with open('input2015_24textsubsets4.txt', 'r') as myfile: #closes by itself ???
+
+with open('input2015_24textsubsets4.txt', 'r') as myfile:
 	text_subsets = myfile.read()
 
 ds = decode_subsets(text_subsets, t)
@@ -187,15 +172,16 @@ for i in sorted_ds:
 			smallests.append(i)
 		else:
 			break
-#print(len(smallest))
+
 
 smallest_qe2 = sys.maxsize
 partition2 = []
 
 for i in smallests:
-	if (functools.reduce(operator.mul, i)):
+	if (functools.reduce(operator.mul, i) < smallest_qe2):
 		smallest_qe2 = functools.reduce(operator.mul, i)
 		partition2 = i
+
 
 print("2. the quantum entanglement of the bag with the smallest ammount of packages equating to the fourth and the smallest entanglement: ", smallest_qe2)
 
--- 2015/2015_24take2.py
+++ 2015/2015_24take2.py
@@ -33,19 +33,27 @@ for i in input:
 t = sum(packages) // 3
 
 
-for i in range(1, len(packages)+1):
-	cp = itertools.combinations(packages, i)
+"""
+we will create all possible combinations of packages of len starting at 1 and onward.
+when we get a combination that sums to t we will accept only combinations of the same len, and none that are longer
+(so it is a brute force, but it stops soon, because we need to find the shortes, thus the first that we find will trigger the end (in some sense))
+after that we just need to find the one with the smalles qe
+"""
+for i in range(1, len(packages)+1): #from 1 to number of packages
+	cp = itertools.combinations(packages, i) #we create combinations (without repetitions) of that length
 
 	found = False
 
 	for j in cp:
-		if (sum(j) == t):
+		if (sum(j) == t): #the first combination with sum of t will trigger this. all combinations of the same len as the this one will string go through
 			found = True
 			sets.append(j)
 	
-	if (found):
+	if (found): #all longer combinations will be rejected
 		break
-	#print(sum((1 for i in cp)))
+	
+
+#we dont need to sort those combinations because we already have just the smallest one, all that reamins is to get the one withe the smalles qe, which is done same as in take 1
 
 
 smallest_qe = sys.maxsize
@@ -59,7 +67,7 @@ for i in sets:
 print("1. the quantum entanglement of the bag with the smallest ammount of packages equating to the third and the smallest entanglement: ", smallest_qe)
 
 
-#second part
+#second part (just different t, all done again)
 t = sum(packages) // 4
 
 
@@ -75,7 +83,6 @@ for i in range(1, len(packages)+1):
 	
 	if (found):
 		break
-	#print(sum((1 for i in cp)))
 
 
 smallest_qe = sys.maxsize
