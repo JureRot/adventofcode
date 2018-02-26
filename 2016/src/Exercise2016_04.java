65e998aa49335059e31056636c718a7095af9485
NeumnoTele <jure.rot96@gmail.com>
2018-02-26 18:16:08 +0100

    2016_04 some

--- 2016/src/Exercise2016_04.java
+++ 2016/src/Exercise2016_04.java
@@ -1,17 +1,51 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Exercise2016_04 {
+
     public static boolean isReal(String name, String checksum) {
         //if checksum is orderd by number of appearences, secundarily alphabetically
 
         //idea, create checksum by the rules, compare (instead of actully checking if checksum is correct)
         //for creating use Map (HashMap) to store number of appearences of letters, than get keys, and sort by value ??? (dont know how yet)
 
-        return true;
+        Map<Character, Integer> letters = new TreeMap<>();
+
+        for (int i=0; i<name.length(); i++) {
+            letters.put(name.charAt(i), 0);
+        }
+        for (int i=0; i<name.length(); i++) {
+            letters.put(name.charAt(i), letters.get(name.charAt(i)) + 1);
+        }
+
+        /*Set<Character> k = letters.keySet();
+        Collection<Integer> v = letters.values();
+        Set<Map.Entry<Character, Integer>> kv = letters.entrySet();*/
+
+        //Map<Integer, ArrayList<Character>> repetitions = new TreeMap<>(); //we could use arraylist
+        Map<Integer, char[]> repetitions = new TreeMap<>();
+
+        for (Map.Entry<Character,Integer> kv : letters.entrySet()) {
+            //repetitions.put(kv.getValue(), new ArrayList<>());
+            repetitions.put(kv.getValue(), new char[]{});
+        }
+        for (Map.Entry<Character,Integer> kv : letters.entrySet()){
+            System.out.println(kv.getKey() +" "+ kv.getValue());
+            /*ArrayList<Character> newValue = new ArrayList<>(repetitions.get(kv.getValue()));
+            newValue.add(kv.getKey());
+            repetitions.put(kv.getValue(), newValue);*/
+            //CREATE NEW CHAR[] ARRAY FROM KV.GETVALUE() AND ADD ADIDITONAL LETTER???
+            char [] newValue = new char[repetitions.get(kv.getValue()).length+1];
+            for (int i=0; i<repetitions.get(kv.getValue()).length; i++) {
+                newValue[i] = repetitions.get(kv.getValue())[i];
+            }
+            newValue[newValue.length-1] = kv.getKey();
+            repetitions.put(kv.getValue(), newValue);
+
+        }
+
+        return false;
     }
 
     public static void main(String[] args) throws FileNotFoundException {
