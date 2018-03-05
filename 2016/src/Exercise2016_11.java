ff0de90252db5e39f222824f93a736e299e29158
NeumnoTele <jure.rot96@gmail.com>
2018-03-05 01:49:53 +0100

    2016_11 idea

--- 2016/src/Exercise2016_11.java
+++ 2016/src/Exercise2016_11.java
@@ -3,6 +3,12 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+class Node {
+    Node() {
+
+    }
+}
+
 public class Exercise2016_11 {
     public static void main(String[] args) throws FileNotFoundException {
 
@@ -16,5 +22,13 @@ public class Exercise2016_11 {
         sc.close();
 
         //idea: implement A* algorithm of searching the ideal path (or at least try to if you get a chance)
+        /*
+        we have a map of all nodes ((sorted) array for every floor, len (to get to node from S), total (value of len + heuristic),
+        previous node)
+        we will probably need more than one map (current for leaves and normal for all)
+        map can be TreeMap and key number ("sorded" by time added)
+        for every iteration we develop the node with the smallest total value -> combinations with len 2 for down and up (if both possible)
+        we continue by A* rules and hope for the best
+         */
     }
 }
