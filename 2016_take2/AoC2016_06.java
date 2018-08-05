0c036992c537be6cde00f1d8a4e7384b0766a348
NeumnoTele <jure.rot96@gmail.com>
2018-08-05 02:46:21 +0200

    2016_06 just created files

---
+++ 2016_take2/AoC2016_06.java
@@ -0,0 +1,10 @@
+class AoC2016_06 {
+    public static void main(String[] args) {
+        long startTime = System.nanoTime();
+
+        
+
+        long endTime = System.nanoTime();
+        System.out.println("Time: " + Double.toString((endTime-startTime)/1000000000.0) + " s");
+    }
+}
