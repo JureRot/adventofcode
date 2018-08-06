8732a26d26245f65debb05abb36b5879e05d9fd4
NeumnoTele <jure.rot96@gmail.com>
2018-08-06 02:28:36 +0200

    2016_07 begining

---
+++ 2016_take2/AoC2016_07.java
@@ -0,0 +1,8 @@
+class AoC2016_07 {
+    public static void main(String[] args) {
+        long startTime = System.nanoTime();
+
+        long endTime = System.nanoTime();
+        System.out.println("Time: " + Double.toString((endTime-startTime)/1000000000.0));
+    }
+}
