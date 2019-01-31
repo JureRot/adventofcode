27afe5de9200f265cf59ecec4ad52ec7647e9198
NeumnoTele <jure.rot96@gmail.com>
2019-01-31 19:46:34 +0100

    15 half-done

---
+++ 2017/AoC2017_15.r
@@ -0,0 +1,45 @@
+input_file = file("input2017_15.txt", "r")
+input <- ""
+while ( TRUE ) {
+  line = readLines(input_file, n = 1)
+  if ( length(line) == 0 ) {
+    break
+  }
+  
+  if (input[1] == "") {
+    input <- line
+  } else {
+    input <- c(input, line)  
+  }
+  
+}
+close(input_file)
+
+#install.packages("R.utils") #if needed install
+library(R.utils)
+
+input <- c("Generator A starts with 65", "Generator B starts with 8921") #test input
+
+a <- as.integer(unlist(strsplit(input[1], " "))[5])
+b <- as.integer(unlist(strsplit(input[2], " "))[5])
+
+match <- 0
+
+for (i in 1:40000000) {
+  a <- (a * 16807) %% 2147483647
+  b <- (b * 48271) %% 2147483647
+  
+  a_str <- intToBin(a) #convert int to bin string
+  a_str <- substr(a_str,(nchar(a_str)+1)-16, nchar(a_str)) #take only the last 16 chars
+  b_str <- intToBin(b)
+  b_str <- substr(b_str,(nchar(b_str)+1)-16, nchar(b_str))
+  
+  if (a_str == b_str) {
+    match <- match + 1
+  }
+  
+  
+  #DONT STRESS, TAKE A BREATH, TAKES A WHILE
+}
+
+match
