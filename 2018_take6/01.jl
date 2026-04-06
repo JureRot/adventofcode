6512f72a04c8001765c5cd90803dd07460b64112
Jure Rot <jure.rot96@gmail.com>
2026-04-06 23:28:17 +0200

    started aoc 2018 (again again). Using Julia (again). aoc 01

---
+++ 2018_take6/01.jl
@@ -0,0 +1,43 @@
+start_time = time();
+
+input_file = "01.txt"
+
+# part 1
+frequency = 0
+
+for line in eachline(input_file)
+	if (first(line) == '-')
+		global frequency -= parse(Int, line[2:end]);
+	else
+		global frequency += parse(Int, line[2:end]);
+	end
+end
+
+println("Part 1: ", frequency)
+
+# part 2
+frequency2 = 0
+found_frequencies = Set(frequency2)
+found = false
+
+while (!found)
+	for line in eachline(input_file)
+		if (first(line) == '-')
+			global frequency2 -= parse(Int, line[2:end]);
+		else
+			global frequency2 += parse(Int, line[2:end]);
+		end
+
+		if (frequency2 in found_frequencies)
+			global found = true;
+			break;
+		else
+			push!(found_frequencies, frequency2);
+		end
+	end
+end
+
+println("Part 2: ", frequency2);
+
+elapsed_time = time() - start_time;
+println("Elapsed time: ", elapsed_time, " s");
